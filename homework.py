d8113ce34a4ae7007ae80db21e894a3e05b106c5
OlegZakharov <varahas@mail.ru>
2022-09-28 21:15:40 +0300

    added task 3 of homework to seminar 4

--- homework.py
+++ homework.py
@@ -3,6 +3,10 @@
 # *Пример:* 
 # - при $d = 0.001, π = 3.141.$
 
+n = 100
+pi = sum(1/16**x*(4/(8*x + 1) - 2/(8*x + 4) - 1/(8*x + 5) - 1/(8*x + 6)) for x in range(n))
+print(round(pi,3))
+
 # Задача2
 #  Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 
@@ -19,10 +23,10 @@
 # Задача3
 # Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
 
-def newList(list):
-    uniqueList = [i for i in list if list.count(i)==1]
-    print(uniqueList)
+# def newList(list):
+#     uniqueList = [i for i in list if list.count(i)==1]
+#     print(uniqueList)
   
-list = [2, 4, 5, 2, 4, 6, 5, 3]
-print(newList(list))
+# list = [2, 4, 5, 2, 4, 6, 5, 3]
+# print(newList(list))
 
